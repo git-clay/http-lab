bf94e6c2dde2d23dc7354302c2c5c66fd0e7260f
git-clay <ce.smith5525@gmail.com>
2016-12-19 16:52:42 -0700

    get post delete

---
+++ starter-code/frontend/js/criminalsController.js
@@ -0,0 +1,44 @@
+angular.module('CriminalApp')
+	.controller('CriminalController', CriminalController);
+
+CriminalController.$inject = ['$http'];
+
+function CriminalController($http){
+	var self = this;
+	self.all = [];
+
+	self.addCriminal = addCriminal;
+	self.newCriminal = {};
+
+	self.getCriminals = getCriminals;
+
+	self.deleteCriminal = deleteCriminal;
+
+	function getCriminals(){
+		$http
+			.get('http://localhost:3000/criminals')
+			.then(function(res){
+				self.all = res.data.criminals;
+			});
+	}
+	getCriminals();
+
+	function addCriminal(){
+		$http
+		.post('http://localhost:3000/criminals',self.newCriminal)
+		.then(function(res){
+			getCriminals();
+		});
+		self.addCriminal = {};
+	}
+	function deleteCriminal(criminal){
+		console.log('delete');
+		$http
+		.delete('http://localhost:3000/criminals/'+criminal._id)
+		.then(function(res){
+			var index = self.all.indexOf(criminal);
+			self.all.splice(index,1);
+		});
+	}
+
+}
